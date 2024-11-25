8f93b5526bcdf48b7a4f92a50dcdf67e92321629
HongTin2104 <kurosakihayato2104@gmail.com>
2024-11-25 08:15:37 +0700

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import requests
 from googletrans import Translator
 
 # Đọc dữ liệu từ file Excel
-file_path = "data.xlsx"
+file_path = "DataSet.xlsx"
 df = pd.read_excel(file_path)
 
 # Tách từ và đếm tần suất
@@ -54,17 +54,38 @@ def recommend_places(input_keywords, df, tfidf, num_recommendations=3):
     recommendations = df.iloc[similar_indices]
     return recommendations
 
+import requests
+from googletrans import Translator
+from unidecode import unidecode  # Import thư viện unidecode
+
 def get_weather(city):
+    # Chuyển tên thành phố về tiếng Việt không dấu
+    city_no_diacritics = unidecode(city)
+    
     BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
     API_KEY = open('api_key', 'r').read().strip()
-    url = BASE_URL + f"appid={API_KEY}&q={city}"
-    response = requests.get(url)
+    
+    # Hàm để lấy dữ liệu thời tiết
+    def fetch_weather(city):
+        url = BASE_URL + f"appid={API_KEY}&q={city}"
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            return response.json()
+        return None
 
+    # Thử lấy thông tin thời tiết cho thành phố đầu tiên (tiếng Việt không dấu)
+    data = fetch_weather(city_no_diacritics)
+    
+    if data is None:
+        # Nếu không tìm thấy, thử lại với tiền tố "Tinh " cho thành phố
+        city_with_prefix = "Tinh " + city_no_diacritics
+        data = fetch_weather(city_with_prefix)
+    
     # Khởi tạo đối tượng dịch
     translator = Translator()
     
-    if response.status_code == 200:
-        data = response.json()
+    if data:
         main = data.get('main', {})
         weather = data.get('weather', [{}])[0]
         temperature = main.get('temp', 0)
@@ -81,6 +102,7 @@ def get_weather(city):
             "humidity": main.get('humidity', 0),
             "icon": f"http://openweathermap.org/img/wn/{icon}@2x.png" if icon else None
         }
+    
     return None
 
 def kelvin_to_celsius_fahrenheit(kelvin):
